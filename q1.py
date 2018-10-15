372f6a5266fe1b472b42708214ee5d8642638f90
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-10-16 00:31:14 +0100

    should work

--- q1.py
+++ q1.py
@@ -6,4 +6,18 @@ import numpy as np
 def question01(portfolios):
   # modify and then return the variable below
   answer = 0
+  ans = []
+  for i in portfolios:
+    for j in portfolios:
+      ans.append(i ^ j)
+  answer = max(ans)
   return answer
+
+def toBinary(n):
+  num = 0
+  power = 0
+  while(n > 0):
+    num += (n % 2) * (10 ** power)
+    n = n/2
+    power += 1
+  return str(num)
