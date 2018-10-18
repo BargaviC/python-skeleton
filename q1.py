e9737ff8768222c6752fe9e8bcc7ff5e83eb7eff
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-10-18 10:00:16 +0100

    modified q1

--- q1.py
+++ q1.py
@@ -9,15 +9,7 @@ def question01(portfolios):
   ans = []
   for i in portfolios:
     for j in portfolios:
-      ans.append(i ^ j)
-  answer = max(ans)
-  return answer
 
-def toBinary(n):
-  num = 0
-  power = 0
-  while(n > 0):
-    num += (n % 2) * (10 ** power)
-    n = n/2
-    power += 1
-  return num
+      if((i ^ j) > answer):
+          answer = i ^ j
+  return answer
--- q3.py
+++ q3.py
@@ -5,4 +5,9 @@ import numpy as np
 def question03(numNodes, edgeList):
   # modify and then return the variable below
   answer = -1
+  xs = set(map(lambda x: x[1], edgeList))
+  ys = set(map(lambda x: x[0], edgeList))
+  print xs, ys
   return answer
+
+question03(3,[(1,2),(2,3)])
