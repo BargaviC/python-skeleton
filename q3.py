73ff06d02b50f8f3aa74b1d14a51c4f042fe0676
Bargavi Chandramouli <bc1417@ic.ac.uk>
2018-10-21 14:00:51 +0100

    q6 trial

--- q3.py
+++ q3.py
@@ -5,9 +5,18 @@ import numpy as np
 def question03(numNodes, edgeList):
   # modify and then return the variable below
   answer = -1
+  nums = set([i for i in range(1, numNodes+1)])
+  print nums
   xs = set(map(lambda x: x[1], edgeList))
   ys = set(map(lambda x: x[0], edgeList))
   print xs, ys
+  excs = {}
+  for i in ys:
+    excs[i] = []
+  for i in edgeList:
+    excs[i[0]].append(i[1])
+  print excs
+
   return answer
 
 question03(3,[(1,2),(2,3)])
--- q6.py
+++ q6.py
@@ -5,4 +5,22 @@ import numpy as np
 def question06(numServers, targetServer, times):
   # modify and then return the variable below
   answer = -1
+  b=[]
+  for i in range(len(times)):
+      bi = []
+      for j in range(len(times[0])):
+
+          if i==j:
+              bi.append(0)
+          elif times[i][j] == 0:
+              bi.append(1000000)
+          else:
+              bi.append(times[i][j])
+      b.append(bi)
+  for k in range(len(times)):
+      for i in range(len(times)):
+          for j in range(len(times)):
+              b[i][j] = min(b[i][j], b[i][k]+b[k][j])
+  answer = b[0][targetServer]
+
   return answer
